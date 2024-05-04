0ff0dc9d935dc3e66b2f517c324e5ff96d142afb
HarineKannan <harinesakthe@gmail.com>
2024-05-04 12:42:26 +0530

    first commit

---
+++ app/components/my-component.js
@@ -0,0 +1,9 @@
+// import Component from '@glimmer/component';
+// import { action } from '@ember/object';
+
+// export default class MyComponent extends Component {
+//     @action
+//     sendDataToParent(data) {
+//         this.args.sendDataToParent(data);
+//     }
+// }
